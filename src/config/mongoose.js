ec5fd9562ffefba9ef1507fc3023b866468d8e83
Elias Andualem <eliasandualem8@gmail.com>
2019-08-24 11:54:02 +0300

    added mongoose config file

---
+++ src/config/mongoose.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const logger = require('./logger');
+const mongoose = require('mongoose');
+const {mongo, env} = require('./env');
+const mongooseHidden = require('mongoose-hidden');
+
+mongoose.Promise = Promise;
+
+if (env === 'development') {
+	mongoose.set('debug', true);
+}
+
+mongoose.plugin(mongooseHidden({
+	defaultHidden: {'_id': false, password: true, pin: true, '__v': true}
+}));
+
+mongoose.connection.on('connected', () => {
+	logger.i('Database Connected.');
+});
+
+mongoose.connection.on('error', err => {
+	logger.e(`Database Connection Failed. ${err}`);
+});
+
+exports.connect = () => {
+	mongoose.connect(mongo.uri, {
+		keepAlive: 1,
+		useNewUrlParser: true,
+	});
+	return mongoose.connection;
+};
